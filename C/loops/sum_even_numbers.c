8925de82b22b73846185ead58fef660dc8aa0691
Md-Dildar <dildar6508@gmail.com>
2026-03-15 21:53:13 +0530

    Add sum_even_numbers program using loops

---
+++ C/loops/sum_even_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i += 2)
+    {
+        sum += i;
+    }
+
+    printf("Sum of even numbers = %d\n", sum);
+
+    return 0;
+}
